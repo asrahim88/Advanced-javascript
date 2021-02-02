fa9c87a5c6a1512d4d92d871c9c51461b8dd511a
asrahim88 <tasmimhasan1997@gmail.com>
2021-02-02 12:49:45 +0600

    nall vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+let number;
+console.log(number);
+
+function add(number1, number2) {
+    const sum = number1 + number2;
+    return
+}
+console.log(add(12, 15));
+
+function add(number1, number2) {
+    const sum = number1 + number2;
+    return
+}
+console.log(add(12));
+
+const students = {name: 'Jhone', Phone: '01774587010', age: 21};
+console.log(students.address);
+
+let fun = undefined;
+console.log(fun);
+
+const age = [12, 34, 65, 25]
+console.log(age[4]);
+
+
