cafdac68ce067bd6a5a408a45786b01b1cbcc5c5
asrahim88 <tasmimhasan1997@gmail.com>
2021-02-02 12:27:48 +0600

    first commit

---
+++ trueFlase.js
@@ -0,0 +1,16 @@
+// falsy
+// false
+// 0
+// ''
+// undefined
+// null
+// NaN
+// Truthy
+// '0', ' ' []
+
+let number = 0;
+if (number || number ==0) {
+    console.log('Condition is true');
+} else {
+    console.log('Condition is false');
+}
