7f35dd8c7d95e023552de1986669896a4b2493d5
asrahim88 <tasmimhasan1997@gmail.com>
2021-02-02 13:08:18 +0600

    double equal And Triple equal

---
+++ double-equal-triple-equal.js
@@ -0,0 +1,99 @@
+// const number = 5;
+// const number1 = 6;
+// if (number == number1) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+// const number2 = 5;
+// const number3 = 5;
+// if (number2 == number3) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+// const number4 = 5;
+// const number5 = "5";
+// if (number4 == number5) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+// const number6 = 5;
+// const number7 = "5";
+// if (number6 === number7) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+// const number8 = 1;
+// const number9 = "false";
+// if (number8 == number9) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+// const number10 = 1;
+// const number11 = false;
+// if (number10 == number11) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+// const number13 = 1;
+// const number14 = true;
+// if (number13 == number14) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+// const number15 = 0;
+// const number16 = true;
+// if (number15 == number16) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+// const number17 = 0;
+// const number18 = false;
+// if (number17 == number18) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+// const number15 = 0;
+// const number16 = false;
+// if (number15 === number16) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+// const number15 = 1;
+// const number16 = true;
+// if (number15 == number16) {
+//     console.log('Condition is true');
+// } else {
+//     console.log('Condition is false');
+// }
+
+const number15 = 1;
+const number16 = true;
+if (number15 === number16) {
+    console.log('Condition is true');
+} else {
+    console.log('Condition is false');
+}
+
+
+
+
